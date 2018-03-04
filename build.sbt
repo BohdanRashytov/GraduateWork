4474ca0edaca3989e9430b5e89d375551bb0f301
Bohdan <rashitov_b@ukr.net>
2018-03-04 14:18:26 +0200

    done

--- build.sbt
+++ build.sbt
@@ -12,7 +12,6 @@ libraryDependencies += "org.scala-lang" % "scala-library" % "2.12.4"
 lazy val root = (project in file("."))
   .settings(name := "root")
   .settings(
-    libraryDependencies += "gov.nist.math" % "jama" % "1.0.3",
     libraryDependencies += "com.github.yannrichet" % "JMathPlot" % "1.0",
     libraryDependencies += "com.github.yannrichet" % "JMathIO" % "1.0",
     libraryDependencies += "com.github.yannrichet" % "JMathArray" % "1.0",
---
+++ src/main/scala/RandomProcess.scala
@@ -0,0 +1,14 @@
+package graduatework
+
+case class RandomProcess(ksi:RandomVariable, eta: RandomVariable, time: Int = GraduateWork.timeCount) {
+  private val count = (ksi.variable.size min eta.variable.size) - 1
+  private val processByTime = (x: Int) => {
+    val iteratorSk = ksi.Sk.toIterator
+    val iteratorEta = eta.variable.toIterator
+    (0 to count).count(_ => iteratorSk.next() + iteratorEta.next() < x)
+  }
+  private val process = (0 to time).map(processByTime(_))
+
+  def getPoints = (0 to time).map(i => (i, process(i))).toList
+
+}
